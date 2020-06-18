1db66910124835f7221dd47ab95bf960b5eff299
miclefeng <miclefengzss@gmail.com>
2020-06-18 14:32:56 +0800

    edit

---
+++ src/main/java/com/synchronizedlearn/SynchronizedLocked.java
@@ -0,0 +1,28 @@
+package com.synchronizedlearn;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SynchronizedLocked {
+
+    Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        SynchronizedLocked s = new SynchronizedLocked();
+        s.method1();
+        s.method2();
+    }
+
+    public synchronized void method1() {
+        System.out.println("synchronized 锁.");
+    }
+
+    public void method2() {
+        lock.lock();
+        try {
+            System.out.println("lock 锁.");
+        } finally {
+            lock.unlock();
+        }
+    }
+}
