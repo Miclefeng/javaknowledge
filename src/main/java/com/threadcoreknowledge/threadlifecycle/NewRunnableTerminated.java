2932926970418dc3b45dc8857bf3519a622f762a
miclefeng <miclefengzss@gmail.com>
2020-06-19 16:08:31 +0800

    edit

---
+++ src/main/java/com/threadcoreknowledge/threadlifecycle/NewRunnableTerminated.java
@@ -0,0 +1,30 @@
+package com.threadcoreknowledge.threadlifecycle;
+
+public class NewRunnableTerminated implements Runnable {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new NewRunnableTerminated());
+        System.out.println(thread.getState());
+        thread.start();
+        System.out.println(thread.getState());
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(thread.getState());
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(thread.getState());
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+        }
+    }
+}
