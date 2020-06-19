9ec16235c9b319214ef8133c817d0c0009dac4a7
miclefeng <miclefengzss@gmail.com>
2020-06-19 16:31:26 +0800

    edit

---
+++ src/main/java/com/threadcoreknowledge/threadlifecycle/BlockedWaitingTimedWaiting.java
@@ -0,0 +1,46 @@
+package com.threadcoreknowledge.threadlifecycle;
+
+/**
+ * blocked  waiting  timed_waiting 3种状态
+ */
+public class BlockedWaitingTimedWaiting implements Runnable {
+
+    public static void main(String[] args) {
+        BlockedWaitingTimedWaiting runnable = new BlockedWaitingTimedWaiting();
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+        t1.start();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // timed_waiting 状态，因为正在执行 Thread.sleep(1000)
+        System.out.println(t1.getState());
+        t2.start();
+        // blocked 状态，因为 t2 无法拿到 synchronized 锁导致
+        System.out.println(t2.getState());
+
+        try {
+            Thread.sleep(1300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // waiting 状态，因为 t1 执行 wait()
+        System.out.println(t1.getState());
+    }
+
+    @Override
+    public void run() {
+        sync();
+    }
+
+    private synchronized void sync() {
+        try {
+            Thread.sleep(1000);
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
