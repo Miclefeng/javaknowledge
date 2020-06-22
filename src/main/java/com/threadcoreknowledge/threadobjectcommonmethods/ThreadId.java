d265881a2aed3fc6e6870e03543dc0c4523a1cae
miclefeng <miclefengzss@gmail.com>
2020-06-22 18:12:52 +0800

    edit

--- src/main/java/com/threadcoreknowledge/threadobjectcommonmethods/ThreadId.java
+++ src/main/java/com/threadcoreknowledge/threadobjectcommonmethods/ThreadId.java
@@ -1,5 +1,8 @@
 package com.threadcoreknowledge.threadobjectcommonmethods;
 
+/**
+ * Id 从1开始，JVM运行起来后会创建和系统相关的线程，所以我们创建的子线程的Id早已不是2.
+ */
 public class ThreadId {
 
     public static void main(String[] args) {
