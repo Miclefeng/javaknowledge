f33f8ca5bd1714d79fa984f6c7ccd3ea599e4f26
miclefeng <miclefengzss@gmail.com>
2020-06-28 18:03:09 +0800

    edit

---
+++ src/main/java/com/basic/knowledge/lambda/Repeat.java
@@ -0,0 +1,24 @@
+package com.basic.knowledge.lambda;
+
+import java.util.function.IntConsumer;
+
+public class Repeat {
+
+    public static void main(String[] args) {
+        Repeat repeat = new Repeat();
+        repeat.repeatRunnable(10, () -> System.out.println("Hello World!"));
+        repeat.repeatConsumer(10, i -> System.out.println("Countdown: " + (9 - i)));
+    }
+
+    public void repeatRunnable(int n, Runnable action) {
+        for (int i = 0; i < n; i++) {
+            action.run();
+        }
+    }
+
+    public void repeatConsumer(int n, IntConsumer action) {
+        for (int i = 0; i < n; i++) {
+            action.accept(i);
+        }
+    }
+}
