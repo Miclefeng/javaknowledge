41644af3b4b20d5f7ee8dd5db481f9a99f66de57
miclefeng <miclefengzss@gmail.com>
2020-06-28 10:11:09 +0800

    edit

--- src/main/java/com/basic/knowledge/reflect/Application.java
+++ src/main/java/com/basic/knowledge/reflect/Application.java
@@ -4,6 +4,15 @@ public class Application {
 
     public static void main(String[] args) {
         String s = "hello world.";
-        new ClassUtil(s).printClassMessage();
+        ClassUtil classUtilString = new ClassUtil(s);
+        classUtilString.printClassMessage();
+        classUtilString.printFieldMessage();
+        classUtilString.printConstructorMessage();
+
+        Integer i = new Integer(1);
+        ClassUtil classUtilInteger = new ClassUtil(i);
+        classUtilInteger.printClassMessage();
+        classUtilInteger.printFieldMessage();
+        classUtilInteger.printConstructorMessage();
     }
 }
--- src/main/java/com/basic/knowledge/reflect/ClassUtil.java
+++ src/main/java/com/basic/knowledge/reflect/ClassUtil.java
@@ -1,17 +1,20 @@
 package com.basic.knowledge.reflect;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class ClassUtil {
 
     private Object object;
+    private final Class<?> c;
 
     public ClassUtil(Object object) {
         this.object = object;
+        this.c = this.object.getClass();
     }
 
     public void printClassMessage() {
-        Class c = this.object.getClass();
         // 获取去类的名称
         System.out.println("类的名称是：" + c.getName());
 
@@ -29,11 +32,45 @@ public class ClassUtil {
             Class<?> methodType = method.getReturnType();
             System.out.print(methodType.getName() + "  ");
             System.out.print(method.getName() + "( ");
-            /**
-             * 获取参数类型
-             */
+            // 获取参数类型
             Class<?>[] parameterTypes = method.getParameterTypes();
-            for (Class parameterType : parameterTypes) {
+            for (Class<?> parameterType : parameterTypes) {
+                System.out.print(parameterType.getName() + ",");
+            }
+            System.out.println(")");
+        }
+    }
+
+    public void printFieldMessage() {
+        System.out.println("类的名称是：" + c.getName());
+        /**
+         * 获取类的域，返回 Field 类
+         * getFields() 获取所有属性为 public 的域
+         * getDeclaredFields() 获取该类自己声明的所有域
+         */
+        Field[] fields = c.getFields();
+        for (Field field : fields
+        ) {
+            // 获取域的类型
+            Class<?> fieldType = field.getType();
+            System.out.print(fieldType.getName() + " ");
+            System.out.println(field.getName());
+        }
+    }
+
+    public void printConstructorMessage() {
+        System.out.println("类的名称是：" + c.getName());
+        /**
+         * 获取类的构造函数信息
+         * getConstructors() 获取所有属性为 public 的构造函数
+         * getDeclaredConstructors() 获取该类自己声明的构造函数
+         */
+        Constructor<?>[] constructors = c.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            String constructorName = constructor.getName();
+            System.out.print(constructorName + "(");
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            for (Class<?> parameterType : parameterTypes) {
                 System.out.print(parameterType.getName() + ",");
             }
             System.out.println(")");
