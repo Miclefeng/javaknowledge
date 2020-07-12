ef7d952ff95ecf3f177d52567b0b54be84693c12
miclefengzss <miclefengzss@163.com>
2020-07-12 12:54:14 +0800

    edit

---
+++ src/main/java/com/basic/knowledge/designpattern/builder/NormalBuilder.java
@@ -0,0 +1,25 @@
+package com.basic.knowledge.designpattern.builder;
+
+/**
+ * @author miclefengzss
+ */
+public class NormalBuilder extends AbstractBuilder {
+
+    @Override
+    public void buildBasic() {
+        house.setBasic("start build normal basic.");
+        System.out.println("normal basic");
+    }
+
+    @Override
+    public void buildWalls() {
+        house.setWall("start build normal wall.");
+        System.out.println("normal wall.");
+    }
+
+    @Override
+    public void roofed() {
+        house.setRoofed("start build normal roofed.");
+        System.out.println("normal roofed");
+    }
+}
