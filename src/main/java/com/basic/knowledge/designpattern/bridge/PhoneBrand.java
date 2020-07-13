f41988914f6fb913aca611a9ef15c38906e9ff21
miclefengzss <miclefengzss@163.com>
2020-07-13 19:21:59 +0800

    edit

---
+++ src/main/java/com/basic/knowledge/designpattern/bridge/PhoneBrand.java
@@ -0,0 +1,13 @@
+package com.basic.knowledge.designpattern.bridge;
+
+/**
+ * @author miclefengzss
+ */
+public interface PhoneBrand {
+
+    void open();
+
+    void call();
+
+    void close();
+}
