bcfbc9a75d703f08edb2ba05aadba66926c2d025
miclefengzss <miclefengzss@163.com>
2020-10-15 14:44:33 +0800

    edit

--- src/main/java/com/basic/knowledge/StringTest.java
+++ src/main/java/com/basic/knowledge/StringTest.java
@@ -1,32 +1,74 @@
 package com.basic.knowledge;
 
+
 public class StringTest {
+
+    private static final char[] HEXCHARS = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+    };
 
     public static void main(String[] args) {
 
-        String hello = "hello world!";
-        // "hello world!" 存储在常量池中， new String() 是 Heap 中的 String 对象
-        String hello1 = new String("hello world!");
-        System.out.println(hello == hello1); //1
+//        String hello = "hello world!";
+//        // "hello world!" 存储在常量池中， new String() 是 Heap 中的 String 对象
+//        String hello1 = new String("hello world!");
+//        System.out.println(hello == hello1); //1
+//
+//        String hello2 = "hello world!";
+//        System.out.println(hello == hello2); //2
+//
+//        // 通过常量表达式运算得到的字符串是在编译时计算得出的，并且之后会将其当作字符串常量对待；
+//        String append = "hello" + " world!";
+//        System.out.println(hello == append); //3
+//
+//        final String pig = "length: 10";
+//        // 在运行时通过连接运算得到的字符串是新创建的
+//        final String dog = "length: " + pig.length();
+//        System.out.println(pig == dog); //4
+//
+//        // intern 方法会从字符串常量池中查询当前字符串是否存在，若不存在就会将当前字符串放入常量池中
+//        final String dog1 = ("length: " + pig.length()).intern();
+//        System.out.println(pig == dog1); //5
+//
+//        // + 的操作优先级高于 ==
+//        // "xxx" + pid == dog  => 用()来改写   ("xxx" + pid) == dog
+//        System.out.println("Animals are equal: " + pig == dog1);//6
+        String str = "9d4f2eabd846478d4f735c33f0a96c363c22fbbfa9ad";
+        System.out.println(str.substring(str.length()-12));
+        System.out.println((int) (System.currentTimeMillis() / 1000));
 
-        String hello2 = "hello world!";
-        System.out.println(hello == hello2); //2
+        String s = "teSt138HellWorld";
+        System.out.println(encodeHexString(s.getBytes()));
 
-        // 通过常量表达式运算得到的字符串是在编译时计算得出的，并且之后会将其当作字符串常量对待；
-        String append = "hello" + " world!";
-        System.out.println(hello == append); //3
 
-        final String pig = "length: 10";
-        // 在运行时通过连接运算得到的字符串是新创建的
-        final String dog = "length: " + pig.length();
-        System.out.println(pig == dog); //4
+        byte[] array = new byte[2];
+        short value = (short) 0x8000;
+        array[0] = (byte) (0xff & value);
+        array[1] = (byte) (0xff & (value >> 8));
 
-        // intern 方法会从字符串常量池中查询当前字符串是否存在，若不存在就会将当前字符串放入常量池中
-        final String dog1 = ("length: " + pig.length()).intern();
-        System.out.println(pig == dog1); //5
+        short dst = (short) (((array[1] & 0xff) << 8) | (array[0] & 0xff));
+        System.out.println(value);
+        System.out.println(dst);
 
-        // + 的操作优先级高于 ==
-        // "xxx" + pid == dog  => 用()来改写   ("xxx" + pid) == dog
-        System.out.println("Animals are equal: " + pig == dog1);//6
+        System.out.println("==============================");
+
+        String s1 = "abc";
+        System.out.println(String.join(".", s1.split("")));
+    }
+
+    /**
+     * Encode the bytes with hex string (lower case)
+     *
+     * @param data
+     * @return
+     */
+    public static String encodeHexString(byte[] data) {
+        StringBuilder sb = new StringBuilder(data.length * 2);
+        for (int i = 0; i < data.length; i++) {
+            sb.append(HEXCHARS[(data[i] & 0xf0) >>> 4]);
+            sb.append(HEXCHARS[data[i] & 0x0f]);
+        }
+        return sb.toString();
     }
 }
+
--- src/main/java/com/rabbitmq/api/exchange/dlx/Consumer.java
+++ src/main/java/com/rabbitmq/api/exchange/dlx/Consumer.java
@@ -1,46 +1,46 @@
-package rabbitmq.api.exchange.dlx;
-
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeoutException;
-
-public class Consumer {
-
-    public static void main(String[] args) throws IOException, TimeoutException {
-
-        ConnectionFactory connectionFactory = new ConnectionFactory();
-        connectionFactory.setHost("127.0.0.1");
-        connectionFactory.setPort(5672);
-        connectionFactory.setVirtualHost("/");
-        connectionFactory.setAutomaticRecoveryEnabled(true);
-        connectionFactory.setNetworkRecoveryInterval(3000);
-
-        Connection connection = connectionFactory.newConnection();
-
-        Channel channel = connection.createChannel();
-
-        String exchangeName = "test_dlx_exchange";
-        String exchangeType = "topic";
-        String routingKey = "dlx.#";
-        String queueName = "test_dlx_exchange";
-        channel.exchangeDeclare(exchangeName, exchangeType, true, false, null);
-
-        Map<String, Object> arguments = new HashMap<>();
-        arguments.put("x-dead-letter-exchange", "dlx.exchange");
-        // 死信队列 arguments 必须在队列上设置
-        channel.queueDeclare(queueName, true, false, false, arguments);
-        channel.queueBind(queueName, exchangeName, routingKey);
-
-        // 死信队列的 exchange 和 queue 声明和绑定
-        channel.exchangeDeclare("dlx.exchange", exchangeType, true, false, null);
-        channel.queueDeclare("dlx.queue", true, false, false, null);
-        channel.queueBind("dlx.queue", "dlx.exchange", "#");
-
-        channel.basicConsume(queueName, false, new MyConsumer(channel));
-    }
-}
+//package rabbitmq.api.exchange.dlx;
+//
+//import com.rabbitmq.client.Channel;
+//import com.rabbitmq.client.Connection;
+//import com.rabbitmq.client.ConnectionFactory;
+//
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.concurrent.TimeoutException;
+//
+//public class Consumer {
+//
+//    public static void main(String[] args) throws IOException, TimeoutException {
+//
+//        ConnectionFactory connectionFactory = new ConnectionFactory();
+//        connectionFactory.setHost("127.0.0.1");
+//        connectionFactory.setPort(5672);
+//        connectionFactory.setVirtualHost("/");
+//        connectionFactory.setAutomaticRecoveryEnabled(true);
+//        connectionFactory.setNetworkRecoveryInterval(3000);
+//
+//        Connection connection = connectionFactory.newConnection();
+//
+//        Channel channel = connection.createChannel();
+//
+//        String exchangeName = "test_dlx_exchange";
+//        String exchangeType = "topic";
+//        String routingKey = "dlx.#";
+//        String queueName = "test_dlx_exchange";
+//        channel.exchangeDeclare(exchangeName, exchangeType, true, false, null);
+//
+//        Map<String, Object> arguments = new HashMap<>();
+//        arguments.put("x-dead-letter-exchange", "dlx.exchange");
+//        // 死信队列 arguments 必须在队列上设置
+//        channel.queueDeclare(queueName, true, false, false, arguments);
+//        channel.queueBind(queueName, exchangeName, routingKey);
+//
+//        // 死信队列的 exchange 和 queue 声明和绑定
+//        channel.exchangeDeclare("dlx.exchange", exchangeType, true, false, null);
+//        channel.queueDeclare("dlx.queue", true, false, false, null);
+//        channel.queueBind("dlx.queue", "dlx.exchange", "#");
+//
+//        channel.basicConsume(queueName, false, new rabbitmq.api.exchange.consumer.MyConsumer(channel));
+//    }
+//}
