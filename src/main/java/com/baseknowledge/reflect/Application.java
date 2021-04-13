daa7905e84e056e3304ee9ebfba7c0b018ee8a39
miclefengzss <miclefengzss@163.com>
2021-04-13 19:52:55 +0800

    reflect

--- src/main/java/com/baseknowledge/reflect/Application.java
+++ src/main/java/com/baseknowledge/reflect/Application.java
@@ -1,6 +1,29 @@
 package com.baseknowledge.reflect;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author miclefengzss
+ */
 public class Application {
+
+    public static void say() {
+        try {
+            // 加载 properties 配置文件
+            Properties properties = new Properties();
+            InputStream resourceAsStream = Application.class.getClassLoader().getResourceAsStream("config.properties");
+            properties.load(resourceAsStream);
+            String language = properties.getProperty("language");
+            System.out.println(language);
+            // 通过反射获取类的实例
+            I18N o = (I18N) Class.forName(language).newInstance();
+            o.say();
+        } catch (IOException | ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         String s = "hello world.";
@@ -14,5 +37,7 @@ public class Application {
         classUtilInteger.printClassMessage();
         classUtilInteger.printFieldMessage();
         classUtilInteger.printConstructorMessage();
+        System.out.println("========================================");
+        say();
     }
 }
---
+++ src/main/java/com/baseknowledge/reflect/I18N.java
@@ -0,0 +1,10 @@
+package com.baseknowledge.reflect;
+
+/**
+ * @author miclefengzss
+ * 2021/4/13 下午7:30
+ */
+public interface I18N {
+
+    void say();
+}
